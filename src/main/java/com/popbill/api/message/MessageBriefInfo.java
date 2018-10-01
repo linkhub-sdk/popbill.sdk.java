e4b0891dc758e2871ab932fd85aff591f65ad713
kimhyunjin <khjzzm@gmail.com>
2018-10-01 10:55:01 +0900

    MSGBriefInfo setter delete

--- src/main/java/com/popbill/api/message/MessageBriefInfo.java
+++ src/main/java/com/popbill/api/message/MessageBriefInfo.java
@@ -24,51 +24,35 @@ public class MessageBriefInfo implements Serializable {
 	public String getSn() {
 		return sn;
 	}
-	public void setSn(String sn) {
-		this.sn = sn;
-	}
+
 	public String getrNum() {
 		return rNum;
 	}
-	public void setrNum(String rNum) {
-		this.rNum = rNum;
-	}
+
 	public String getStat() {
 		return stat;
 	}
-	public void setStat(String stat) {
-		this.stat = stat;
-	}
+
 	public String getsDT() {
 		return sDT;
 	}
-	public void setsDT(String sDT) {
-		this.sDT = sDT;
-	}
+
 	public String getrDT() {
 		return rDT;
 	}
-	public void setrDT(String rDT) {
-		this.rDT = rDT;
-	}
+
 	public String getRlt() {
 		return rlt;
 	}
-	public void setRlt(String rlt) {
-		this.rlt = rlt;
-	}
+
 	public String getNet() {
 		return net;
 	}
-	public void setNet(String net) {
-		this.net = net;
-	}
+
 	public String getSrt() {
 		return srt;
 	}
-	public void setSrt(String srt) {
-		this.srt = srt;
-	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
