653af518c64b1d36fe67975103501bed53457525
JohnJeong123 <frenchofkiss@gmail.com>
2020-11-30 15:57:58 +0900

    전세금 대량접수 상태확인 함수 수정.

--- src/main/java/com/popbill/api/taxinvoice/BulkTaxinvoiceResult.java
+++ src/main/java/com/popbill/api/taxinvoice/BulkTaxinvoiceResult.java
@@ -1,30 +1,76 @@
 package com.popbill.api.taxinvoice;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class BulkTaxinvoiceResult implements Serializable {
 
 	private static final long serialVersionUID = -4224469288700675685L;
 	
-	private long ResultCode;
+	private long Code;
 	private String Message;
+	private String receiptID;
+	private String submitID;
+	private long submitCount;
+	private long successCount;
+	private long failCount;
+	private long txState;
+	private String txStartDT;
+	private String txEndDT;
+	private long txResultCode;
 	
-	private BulkTaxinvoiceResultDetail retObject;
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-	public long getResultCode() {
-		return ResultCode;
+	public long getCode() {
+		return Code;
 	}
 
 	public String getMessage() {
 		return Message;
 	}
+	
+	private List<BulkTaxinvoiceIssueResult> issueResult;
 
-	public BulkTaxinvoiceResultDetail getRetObject() {
-		return retObject;
+	public String getReceiptID() {
+		return receiptID;
+	}
+
+	public String getSubmitID() {
+		return submitID;
+	}
+
+	public long getSubmitCount() {
+		return submitCount;
+	}
+
+	public long getSuccessCount() {
+		return successCount;
+	}
+
+	public long getFailCount() {
+		return failCount;
+	}
+
+	public long getTxState() {
+		return txState;
+	}
+
+	public String getTxStartDT() {
+		return txStartDT;
+	}
+
+	public String getTxEndDT() {
+		return txEndDT;
+	}
+
+	public long getTxResultCode() {
+		return txResultCode;
+	}
+
+	public List<BulkTaxinvoiceIssueResult> getIssueResult() {
+		return issueResult;
 	}
 	
 	
--- src/test/java/com/popbill/api/taxinvoice/test/TaxinvoiceServiceTEST.java
+++ src/test/java/com/popbill/api/taxinvoice/test/TaxinvoiceServiceTEST.java
@@ -2,10 +2,7 @@ package com.popbill.api.taxinvoice.test;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -27,6 +24,7 @@ import com.popbill.api.taxinvoice.EmailPublicKey;
 import com.popbill.api.taxinvoice.MgtKeyType;
 import com.popbill.api.taxinvoice.TISearchResult;
 import com.popbill.api.taxinvoice.Taxinvoice;
+import com.popbill.api.taxinvoice.TaxinvoiceAddContact;
 import com.popbill.api.taxinvoice.TaxinvoiceDetail;
 import com.popbill.api.taxinvoice.TaxinvoiceInfo;
 import com.popbill.api.taxinvoice.TaxinvoiceLog;
@@ -48,16 +46,13 @@ public class TaxinvoiceServiceTEST {
 		service.setUseStaticIP(false);
 		service.setUseLocalTimeYN(false);
 		
-//		service.setProxyIP("192.168.0.215");
-//		service.setProxyPort(8081);
-		
 		taxinvoiceService = service;
 	}
 	
 	@Test
 	public void getChargeInfo_TEST() throws PopbillException {
 		
-		ChargeInfo chrgInfo = taxinvoiceService.getChargeInfo("1234567890");
+		ChargeInfo chrgInfo = taxinvoiceService.getChargeInfo("6798700433");
 
 		System.out.println(chrgInfo.getChargeMethod());
 		System.out.println(chrgInfo.getUnitCost());
@@ -301,8 +296,8 @@ public class TaxinvoiceServiceTEST {
 		taxinvoiceService.setSecretKey(testSecretKey);
 		taxinvoiceService.setTest(true);
 
-		Response response = taxinvoiceService.delete("1231212312",
-				MgtKeyType.SELL, "1234");
+		Response response = taxinvoiceService.delete("1234567890",
+				MgtKeyType.SELL, "20201126-004");
 
 		assertNotNull(response);
 
@@ -383,8 +378,8 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void cancelIssue_TEST() throws PopbillException {
 	
-		Response response = taxinvoiceService.cancelIssue("1231212312",
-				MgtKeyType.SELL, "1234", "발행 취소 메모");
+		Response response = taxinvoiceService.cancelIssue("6798700433",
+				MgtKeyType.SELL, "20201126-004", "cancelIssue memo");
 
 		assertNotNull(response);
 
@@ -431,8 +426,8 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void sendToNTS_TEST() throws PopbillException {
 	
-		Response response = taxinvoiceService.sendToNTS("1231212312",
-				MgtKeyType.SELL, "1234");
+		Response response = taxinvoiceService.sendToNTS("1234567890",
+				MgtKeyType.SELL, "20201126-002d");
 
 		assertNotNull(response);
 
@@ -443,8 +438,8 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void sendEmail_TEST() throws PopbillException {
 	
-		Response response = taxinvoiceService.sendEmail("1231212312",
-				MgtKeyType.SELL, "1234", "test@test.com");
+		Response response = taxinvoiceService.sendEmail("1234567890",
+				MgtKeyType.SELL, "20201126-001d", "resend@email.com");
 
 		assertNotNull(response);
 
@@ -481,7 +476,7 @@ public class TaxinvoiceServiceTEST {
 	public void getInfo_TEST() throws PopbillException {
 	
 		TaxinvoiceInfo taxinvoiceInfo = taxinvoiceService.getInfo("1234567890",
-				MgtKeyType.SELL, "20161221-03");
+				MgtKeyType.SELL, "20201126-003d");
 
 		assertNotNull(taxinvoiceInfo);
 		System.out.println("[getInfo API] - "+taxinvoiceInfo.getCloseDownState()+ " "+taxinvoiceInfo.getInterOPYN());
@@ -490,7 +485,7 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void getDetailInfo_TEST() throws PopbillException {
 			Taxinvoice taxinvoice = taxinvoiceService.getDetailInfo("1234567890",
-				MgtKeyType.SELL, "20161221-03");
+				MgtKeyType.SELL, "20201126-001d");
 
 		assertNotNull(taxinvoice);
 		System.out.println(taxinvoice.getCloseDownState() + " | " + taxinvoice.getCloseDownStateDate());
@@ -592,7 +587,7 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void getInfos_TEST() throws PopbillException {
 		
-		String[] MgtKeyList = new String[] { "20161221-03", "20161221-02", "12345", "20180712_04" };
+		String[] MgtKeyList = new String[] { "20201126-001d" };
 
 		TaxinvoiceInfo[] infoList = taxinvoiceService.getInfos("1234567890",
 				MgtKeyType.SELL, MgtKeyList);
@@ -685,7 +680,7 @@ public class TaxinvoiceServiceTEST {
 	public void RegistIssue_TEST() throws PopbillException {
 		Taxinvoice taxinvoice = new Taxinvoice();
 
-		taxinvoice.setWriteDate("20200827"); // 필수, 기재상 작성일자
+		taxinvoice.setWriteDate("20201126"); // 필수, 기재상 작성일자
 		taxinvoice.setChargeDirection("정과금"); // 필수, {정과금, 역과금}
 		taxinvoice.setIssueType("정발행"); // 필수, {정발행, 역발행, 위수탁}
 		taxinvoice.setPurposeType("영수"); // 필수, {영수, 청구}
@@ -693,41 +688,41 @@ public class TaxinvoiceServiceTEST {
 		taxinvoice.setTaxType("과세"); // 필수, {과세, 영세, 면세}
 		taxinvoice.setInvoicerCorpNum("1234567890");
 		taxinvoice.setInvoicerTaxRegID(""); // 종사업자 식별번호. 필요시 기재. 형식은 숫자 4자리.
-		taxinvoice.setInvoicerCorpName("공급자 상호");
-		taxinvoice.setInvoicerMgtKey("20200827-002"); // 공급자 발행까지 API로 발행하고자 할경우 정발행과
-		taxinvoice.setInvoicerCEOName("공급자 대표자 성명");
-		taxinvoice.setInvoicerAddr("공급자 주소");
-		taxinvoice.setInvoicerBizClass("공급자 업종");
-		taxinvoice.setInvoicerBizType("공급자 업태,업태2");
-		taxinvoice.setInvoicerContactName("공급자 담당자명");
-		taxinvoice.setInvoicerEmail("test@test.com");
+		taxinvoice.setInvoicerCorpName("Seller Name of Company");
+		taxinvoice.setInvoicerMgtKey("20201126-005"); // 공급자 발행까지 API로 발행하고자 할경우 정발행과
+		taxinvoice.setInvoicerCEOName("Seller Name of Representative");
+		taxinvoice.setInvoicerAddr("Seller Company Address");
+		taxinvoice.setInvoicerBizClass("Seller Business Item");
+		taxinvoice.setInvoicerBizType("Seller Business Type");
+		taxinvoice.setInvoicerContactName("Seller Name of the person in charge");
+		taxinvoice.setInvoicerEmail("seller@email.com");
 		taxinvoice.setInvoicerTEL("070-7070-0707");
-		taxinvoice.setInvoicerHP("010-000-2222");
 		taxinvoice.setInvoicerSMSSendYN(false); // 발행시 문자발송기능 사용시 활용
 		
 		taxinvoice.setInvoiceeType("사업자");
 		taxinvoice.setInvoiceeCorpNum("8888888888");
-		taxinvoice.setInvoiceeCorpName("공급받는자 상호");
+		taxinvoice.setInvoiceeCorpName("Buyer Name of Company");
 		taxinvoice.setInvoiceeMgtKey(null); // 문서관리번호 1~24자리까지 공급받는자 사업자번호별 중복없는
-		taxinvoice.setInvoiceeCEOName("공급받는자 대표자 성명");
-		taxinvoice.setInvoiceeAddr("공급받는자 주소");
-		taxinvoice.setInvoiceeBizClass("공급받는자 업종");
-		taxinvoice.setInvoiceeBizType("공급받는자 업태");
-		taxinvoice.setInvoiceeContactName1("공급받는자 담당자명");
-		taxinvoice.setInvoiceeEmail1("test@naver.com");
-		taxinvoice.setSupplyCostTotal("100000"); // 필수 공급가액 합계"
-		taxinvoice.setTaxTotal("10000"); // 필수 세액 합계
-		taxinvoice.setTotalAmount("110000"); // 필수 합계금액. 공급가액 + 세액
-		taxinvoice.setModifyCode((short)1); // 수정세금계산서 작성시 1~6까지 선택기재.
-		taxinvoice.setOrgNTSConfirmNum("20191008888888880000003e"); // 수정세금계산서 작성시 원본세금계산서의 ItemKey기재. ItemKey는 문서확인.
-		taxinvoice.setSerialNum("123");
+		taxinvoice.setInvoiceeCEOName("Buyer Name of Representative");
+		taxinvoice.setInvoiceeAddr("Buyer Company Address");
+		taxinvoice.setInvoiceeBizClass("Buyer Business Item");
+		taxinvoice.setInvoiceeBizType("Buyer Business Type");
+		taxinvoice.setInvoiceeContactName1("Buyer Name of the person in charge");
+		taxinvoice.setInvoiceeEmail1("buyer@email.com");
+		taxinvoice.setInvoiceeTEL1("070-7070-3456");
+		taxinvoice.setSupplyCostTotal("200000"); // 필수 공급가액 합계"
+		taxinvoice.setTaxTotal("20000"); // 필수 세액 합계
+		taxinvoice.setTotalAmount("220000"); // 필수 합계금액. 공급가액 + 세액
+//		taxinvoice.setModifyCode((short)1); // 수정세금계산서 작성시 1~6까지 선택기재.
+//		taxinvoice.setOrgNTSConfirmNum("20191008888888880000003e"); // 수정세금계산서 작성시 원본세금계산서의 ItemKey기재. ItemKey는 문서확인.
+		taxinvoice.setSerialNum("");
 		taxinvoice.setCash(""); // 현금
 		taxinvoice.setChkBill(""); // 수표
 		taxinvoice.setNote(""); // 어음
 		taxinvoice.setCredit(""); // 외상미수금
-		taxinvoice.setRemark1("비고1");
-//		taxinvoice.setRemark2("비고2");
-//		taxinvoice.setRemark3("비고3");
+		taxinvoice.setRemark1("remark 01");
+		taxinvoice.setRemark2("remark 02");
+		taxinvoice.setRemark3("remark 03");
 		taxinvoice.setKwon((short) 1);
 		taxinvoice.setHo((short) 1);
 		taxinvoice.setBusinessLicenseYN(false); // 사업자등록증 이미지 첨부시 설정.
@@ -738,9 +733,9 @@ public class TaxinvoiceServiceTEST {
 		TaxinvoiceDetail detail = new TaxinvoiceDetail();
 
 		detail.setSerialNum((short) 1); // 일련번호
-		detail.setPurchaseDT("20190226"); // 거래일자
-		detail.setItemName("품목명");
-		detail.setSpec("규격");
+		detail.setPurchaseDT("20201126"); // 거래일자
+		detail.setItemName("ItemName01");
+		detail.setSpec("spec01");
 		detail.setQty("1"); // 수량
 		detail.setUnitCost("100000"); // 단가
 		detail.setSupplyCost("100000"); // 공급가액
@@ -750,10 +745,34 @@ public class TaxinvoiceServiceTEST {
 
 		detail = new TaxinvoiceDetail();
 		detail.setSerialNum((short) 2);
-		detail.setItemName("품목명");
+		detail.setPurchaseDT("20201126"); // 거래일자
+		detail.setItemName("ItemName02");
+		detail.setSpec("spec02");
+		detail.setQty("1"); // 수량
+		detail.setUnitCost("100000"); // 단가
+		detail.setSupplyCost("100000"); // 공급가액
+		detail.setTax("10000"); // 세액
+		detail.setRemark("품목비고");
 		taxinvoice.getDetailList().add(detail);
+		
+		taxinvoice.setAddContactList(new ArrayList<TaxinvoiceAddContact>());
+		
+		TaxinvoiceAddContact contact = new TaxinvoiceAddContact();
+		contact.setSerialNum((short)1);
+		contact.setContactName("Additional Contactor 01");
+		contact.setEmail("addcontact01@test.com");
+		
+		taxinvoice.getAddContactList().add(contact);
+		
+		contact = new TaxinvoiceAddContact();
+		contact.setSerialNum((short)2);
+		contact.setContactName("Additional Contactor 02");
+		contact.setEmail("addcontact02@test.com");
+		
+		taxinvoice.getAddContactList().add(contact);
 
-		IssueResponse response = taxinvoiceService.registIssue("1234567890", taxinvoice, true, "즉시발행 메모야", true, null, "메일제목이라네", "testkorea");
+		
+		IssueResponse response = taxinvoiceService.registIssue("1234567890", taxinvoice, false, "memo", false, null, "Custom Email Title", "");
 		
 		assertNotNull(response);
 
@@ -763,27 +782,27 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void getBulkResult_TEST() throws PopbillException {
 	
-		BulkTaxinvoiceResult bulkResult = taxinvoiceService.getBulkResult("6798700433", "20200924-001", "linkhub");
+		BulkTaxinvoiceResult bulkResult = taxinvoiceService.getBulkResult("1234567890", "20201126-0022", "");
 		
 		assertNotNull(bulkResult);
 		
-		System.out.println(bulkResult.getResultCode());
+		System.out.println(bulkResult.getCode());
 		System.out.println(bulkResult.getMessage());
-		System.out.println(bulkResult.getRetObject().getReceiptID());
-		System.out.println(bulkResult.getRetObject().getSubmitID());
-		System.out.println(bulkResult.getRetObject().getSubmitCount());
-		System.out.println(bulkResult.getRetObject().getSuccessCount());
-		System.out.println(bulkResult.getRetObject().getFailCount());
-		System.out.println(bulkResult.getRetObject().getTxState());
-		System.out.println(bulkResult.getRetObject().getTxStartDT());
-		System.out.println(bulkResult.getRetObject().getTxEndDT());
-		System.out.println(bulkResult.getRetObject().getTxResultCode());
+		System.out.println(bulkResult.getReceiptID());
+		System.out.println(bulkResult.getSubmitID());
+		System.out.println(bulkResult.getSubmitCount());
+		System.out.println(bulkResult.getSuccessCount());
+		System.out.println(bulkResult.getFailCount());
+		System.out.println(bulkResult.getTxState());
+		System.out.println(bulkResult.getTxStartDT());
+		System.out.println(bulkResult.getTxEndDT());
+		System.out.println(bulkResult.getTxResultCode());
 		
-		for(int i=0; i< bulkResult.getRetObject().getIssueResult().size(); i++) {
+		for(int i=0; i< bulkResult.getIssueResult().size(); i++) {
 			
-			String n1 = bulkResult.getRetObject().getIssueResult().get(i).getInvoicerMgtKey();
-			long n2 = bulkResult.getRetObject().getIssueResult().get(i).getCode();
-			String n3 = bulkResult.getRetObject().getIssueResult().get(i).getConfirmNumber();
+			String n1 = bulkResult.getIssueResult().get(i).getInvoicerMgtKey();
+			long n2 = bulkResult.getIssueResult().get(i).getCode();
+			String n3 = bulkResult.getIssueResult().get(i).getConfirmNumber();
 			System.out.println(n1+ " " + String.valueOf(n2) +" "+n3);
 		}		
 	}
@@ -793,55 +812,58 @@ public class TaxinvoiceServiceTEST {
 		
 		List<Taxinvoice> bulkTx = new ArrayList<Taxinvoice>();
 		
-		String SubmitID = "20200924-009";
+		String SubmitID = "20201126-006";
 		
+		for(int j=0; j<1; j++) {
+			//SubmitID += String.valueOf(j);
+			bulkTx = new ArrayList<Taxinvoice>();
 		for(int i=0; i<100; i++) {
+			
 			Taxinvoice taxinvoice = new Taxinvoice();
 
-			taxinvoice.setWriteDate("20200924"); // 필수, 기재상 작성일자
+			taxinvoice.setWriteDate("20201126"); // 필수, 기재상 작성일자
 			taxinvoice.setChargeDirection("정과금"); // 필수, {정과금, 역과금}
 			taxinvoice.setIssueType("정발행"); // 필수, {정발행, 역발행, 위수탁}
 			taxinvoice.setPurposeType("영수"); // 필수, {영수, 청구}
 			taxinvoice.setIssueTiming("직접발행"); // 필수, {직접발행, 승인시자동발행}
 			taxinvoice.setTaxType("과세"); // 필수, {과세, 영세, 면세}
-			
-			taxinvoice.setInvoicerCorpNum("6798700433");
+			taxinvoice.setInvoicerCorpNum("1234567890");
 			taxinvoice.setInvoicerTaxRegID(""); // 종사업자 식별번호. 필요시 기재. 형식은 숫자 4자리.
-			taxinvoice.setInvoicerCorpName("공급자 상호");
+			taxinvoice.setInvoicerCorpName("Seller Name of Company");
 			taxinvoice.setInvoicerMgtKey(SubmitID+"-"+ String.valueOf(i)); // 공급자 발행까지 API로 발행하고자 할경우 정발행과
-			taxinvoice.setInvoicerCEOName("공급자 대표자 성명");
-			taxinvoice.setInvoicerAddr("공급자 주소");
-			taxinvoice.setInvoicerBizClass("공급자 업종");
-			taxinvoice.setInvoicerBizType("공급자 업태,업태2");
-			taxinvoice.setInvoicerContactName("공급자 담당자명");
-			taxinvoice.setInvoicerEmail("test@test.com");
+			taxinvoice.setInvoicerCEOName("Seller Name of Representative");
+			taxinvoice.setInvoicerAddr("Seller Company Address");
+			taxinvoice.setInvoicerBizClass("Seller Business Item");
+			taxinvoice.setInvoicerBizType("Seller Business Type");
+			taxinvoice.setInvoicerContactName("Seller Name of the person in charge");
+			taxinvoice.setInvoicerEmail("seller@email.com");
 			taxinvoice.setInvoicerTEL("070-7070-0707");
-			taxinvoice.setInvoicerHP("010-000-2222");
 			taxinvoice.setInvoicerSMSSendYN(false); // 발행시 문자발송기능 사용시 활용
 			
 			taxinvoice.setInvoiceeType("사업자");
 			taxinvoice.setInvoiceeCorpNum("8888888888");
-			taxinvoice.setInvoiceeCorpName("공급받는자 상호");
+			taxinvoice.setInvoiceeCorpName("Buyer Name of Company");
 			taxinvoice.setInvoiceeMgtKey(null); // 문서관리번호 1~24자리까지 공급받는자 사업자번호별 중복없는
-			taxinvoice.setInvoiceeCEOName("공급받는자 대표자 성명");
-			taxinvoice.setInvoiceeAddr("공급받는자 주소");
-			taxinvoice.setInvoiceeBizClass("공급받는자 업종");
-			taxinvoice.setInvoiceeBizType("공급받는자 업태");
-			taxinvoice.setInvoiceeContactName1("공급받는자 담당자명");
-			taxinvoice.setInvoiceeEmail1("");
-			taxinvoice.setSupplyCostTotal("100000"); // 필수 공급가액 합계"
-			taxinvoice.setTaxTotal("10000"); // 필수 세액 합계
-			taxinvoice.setTotalAmount("110000"); // 필수 합계금액. 공급가액 + 세액
+			taxinvoice.setInvoiceeCEOName("Buyer Name of Representative");
+			taxinvoice.setInvoiceeAddr("Buyer Company Address");
+			taxinvoice.setInvoiceeBizClass("Buyer Business Item");
+			taxinvoice.setInvoiceeBizType("Buyer Business Type");
+			taxinvoice.setInvoiceeContactName1("Buyer Name of the person in charge");
+			taxinvoice.setInvoiceeEmail1("buyer@email.com");
+			taxinvoice.setInvoiceeTEL1("070-7070-3456");
+			taxinvoice.setSupplyCostTotal("200000"); // 필수 공급가액 합계"
+			taxinvoice.setTaxTotal("20000"); // 필수 세액 합계
+			taxinvoice.setTotalAmount("220000"); // 필수 합계금액. 공급가액 + 세액
 //			taxinvoice.setModifyCode((short)1); // 수정세금계산서 작성시 1~6까지 선택기재.
 //			taxinvoice.setOrgNTSConfirmNum("20191008888888880000003e"); // 수정세금계산서 작성시 원본세금계산서의 ItemKey기재. ItemKey는 문서확인.
-			taxinvoice.setSerialNum("123");
+			taxinvoice.setSerialNum("");
 			taxinvoice.setCash(""); // 현금
 			taxinvoice.setChkBill(""); // 수표
 			taxinvoice.setNote(""); // 어음
 			taxinvoice.setCredit(""); // 외상미수금
-			taxinvoice.setRemark1("비고1");
-			taxinvoice.setRemark2("비고2");
-			taxinvoice.setRemark3("비고3");
+			taxinvoice.setRemark1("remark 01");
+			taxinvoice.setRemark2("remark 02");
+			taxinvoice.setRemark3("remark 03");
 			taxinvoice.setKwon((short) 1);
 			taxinvoice.setHo((short) 1);
 			taxinvoice.setBusinessLicenseYN(false); // 사업자등록증 이미지 첨부시 설정.
@@ -852,9 +874,9 @@ public class TaxinvoiceServiceTEST {
 			TaxinvoiceDetail detail = new TaxinvoiceDetail();
 
 			detail.setSerialNum((short) 1); // 일련번호
-			detail.setPurchaseDT("20200924"); // 거래일자
-			detail.setItemName("품목명");
-			detail.setSpec("규격");
+			detail.setPurchaseDT("20201126"); // 거래일자
+			detail.setItemName("ItemName01");
+			detail.setSpec("spec01");
 			detail.setQty("1"); // 수량
 			detail.setUnitCost("100000"); // 단가
 			detail.setSupplyCost("100000"); // 공급가액
@@ -864,18 +886,46 @@ public class TaxinvoiceServiceTEST {
 
 			detail = new TaxinvoiceDetail();
 			detail.setSerialNum((short) 2);
-			detail.setItemName("품목명");
+			detail.setPurchaseDT("20201126"); // 거래일자
+			detail.setItemName("ItemName02");
+			detail.setSpec("spec02");
+			detail.setQty("1"); // 수량
+			detail.setUnitCost("100000"); // 단가
+			detail.setSupplyCost("100000"); // 공급가액
+			detail.setTax("10000"); // 세액
+			detail.setRemark("품목비고");
 			taxinvoice.getDetailList().add(detail);
+			
+			taxinvoice.setAddContactList(new ArrayList<TaxinvoiceAddContact>());
+			
+			TaxinvoiceAddContact contact = new TaxinvoiceAddContact();
+			contact.setSerialNum((short)1);
+			contact.setContactName("Additional Contactor 01");
+			contact.setEmail("addcontact01@test.com");
+			
+			taxinvoice.getAddContactList().add(contact);
+			
+			contact = new TaxinvoiceAddContact();
+			contact.setSerialNum((short)2);
+			contact.setContactName("Additional Contactor 02");
+			contact.setEmail("addcontact02@test.com");
+			
+			taxinvoice.getAddContactList().add(contact);
+			
+			
+			
+			
 			
 			bulkTx.add(taxinvoice);
 		}
 		
 
-		BulkResponse response = taxinvoiceService.bulkSubmit("6798700433", SubmitID, bulkTx, false, "linkhub");
+		BulkResponse response = taxinvoiceService.bulkSubmit("1234567890", SubmitID, bulkTx, false, "");
 		
 		assertNotNull(response);
 
 		System.out.println("["+response.getCode()+"] "+response.getMessage()+" : "+response.getReceiptID());
+		}
 	}
 	
 	@Test
@@ -1064,26 +1114,26 @@ public class TaxinvoiceServiceTEST {
 	@Test
 	public void GetPDF_TEST() throws PopbillException {
 		
-		byte[] pdfByte = taxinvoiceService.getPDF("1234567890", MgtKeyType.SELL, "20200806-01 ");
+		byte[] pdfByte = taxinvoiceService.getPDF("1234567890", MgtKeyType.SELL, "20201126-001 ");
 		
-		File outfile = new File("C:/pdf_test/PDF_Update/20200901_Taxinvoice_TEST_T1.pdf");
-		FileOutputStream fileoutputstream;
-		try {
-			fileoutputstream = new FileOutputStream(outfile);
-			try {
-				fileoutputstream.write(pdfByte);
-				System.out.println("다운로드 성공");
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			try {
-				fileoutputstream.close();
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+//		File outfile = new File("C:/pdf_test/PDF_Update/20200901_Taxinvoice_TEST_T1.pdf");
+//		FileOutputStream fileoutputstream;
+//		try {
+//			fileoutputstream = new FileOutputStream(outfile);
+//			try {
+//				fileoutputstream.write(pdfByte);
+//				System.out.println("다운로드 성공");
+//			} catch (IOException e1) {
+//				e1.printStackTrace();
+//			}
+//			try {
+//				fileoutputstream.close();
+//			} catch (IOException e2) {
+//				e2.printStackTrace();
+//			}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
