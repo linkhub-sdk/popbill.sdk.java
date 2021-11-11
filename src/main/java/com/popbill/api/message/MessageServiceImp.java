f3b2c660f38e7faa2ca8b30c0f70904f783e5840
WonJae <wjkim@linkhub.co.kr>
2021-11-11 14:20:19 +0900

    added FileInputStream close()

--- src/main/java/com/popbill/api/message/MessageServiceImp.java
+++ src/main/java/com/popbill/api/message/MessageServiceImp.java
@@ -17,6 +17,7 @@ package com.popbill.api.message;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -831,6 +832,11 @@ public class MessageServiceImp extends BaseServiceImp implements MessageService 
 
         ReceiptResponse response = httppostFiles("/MMS", CorpNum,
                 PostData, uploadFiles, UserID, ReceiptResponse.class);
+        
+        if (uf.fileData != null)
+            try {
+                uf.fileData.close();
+            } catch (IOException e) {}
 
         return response.receiptNum;
     }
@@ -963,6 +969,11 @@ public class MessageServiceImp extends BaseServiceImp implements MessageService 
 
         ReceiptResponse response = httppostFiles("/MMS", CorpNum,
                 PostData, uploadFiles, UserID, ReceiptResponse.class);
+        
+        if (uf.fileData != null)
+            try {
+                uf.fileData.close();
+            } catch (IOException e) {}
 
         return response.receiptNum;
     }
